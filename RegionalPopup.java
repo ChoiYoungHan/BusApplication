498d9a1fd33ac8411d20069949bdf8cba2e31616
ChoiYoungHan <102001212+ChoiYoungHan@users.noreply.github.com>
2022-06-05 20:02:43 +0900

    Create RegionalPopup.java
    팝업 화면용

---
+++ RegionalPopup.java
@@ -0,0 +1,14 @@
+package com.example.mainproject0531;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegionalPopup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regional_popup);
+    }
+}
