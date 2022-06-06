cbb58e8dbac91c6dc00b46c326b4f48551e43d7c
ChoiYoungHan <102001212+ChoiYoungHan@users.noreply.github.com>
2022-06-06 18:21:13 +0900

    Create CallbenNumber.java

---
+++ CallbenNumber.java
@@ -0,0 +1,14 @@
+package com.example.mainproject0531;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CallbenNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_callben_number);
+    }
+}
