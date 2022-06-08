2650a6146cce8ab15712107d8f03310ed239bf0e
ChoiYoungHan <102001212+ChoiYoungHan@users.noreply.github.com>
2022-06-08 16:34:29 +0900

    Create PostList.java

---
+++ PostList.java
@@ -0,0 +1,14 @@
+package com.example.mainproject0531;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_list);
+    }
+}
